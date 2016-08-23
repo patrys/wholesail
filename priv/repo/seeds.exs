ddbbd93bdc0e6b3f5f22577f79a912c7f44bc449
Patryk Zawadzki <patrys@room-303.com>
2016-08-23 19:54:05 +0200

    Start building a seed file for the repo
    Fixes #8

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,12 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+shirts = %Wholesail.Category{}
+|> Wholesail.Category.changeset(%{name: "Shirts"})
+|> Wholesail.Repo.insert!
+
+shirt = %Wholesail.Product{}
+|> Wholesail.Product.changeset(%{name: "Shirt", description: "A shirt.", price: "12.45"})
+|> Ecto.Changeset.put_assoc(:category, shirts)
+|> Ecto.Changeset.put_assoc(:variants, [%{sku: "1234567890123"}])
+|> Wholesail.Repo.insert!
